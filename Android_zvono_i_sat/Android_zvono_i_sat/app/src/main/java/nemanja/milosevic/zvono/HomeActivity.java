69a1b1a64d80c9715944d4bfd1405f26f24b0d6c
Nemanja Milošević <nemanjamnn02@gmail.com>
2024-12-28 19:39:02 +0100

    28.12.2024.

--- Android_zvono_i_sat/Android_zvono_i_sat/app/src/main/java/nemanja/milosevic/zvono/HomeActivity.java
+++ Android_zvono_i_sat/Android_zvono_i_sat/app/src/main/java/nemanja/milosevic/zvono/HomeActivity.java
@@ -1,13 +1,17 @@
 package nemanja.milosevic.zvono;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
-
+import android.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -21,12 +25,29 @@ public class HomeActivity extends AppCompatActivity {
 
         ImageButton imgBtnWifi = findViewById(R.id.imgBtnWifi);
         ImageButton imgBtnBell = findViewById(R.id.imgBtnBell);
+        ImageButton imgBtnConfirm = findViewById(R.id.imgBtnCorrect);
+        ImageButton imgBtnConfirmRouter = findViewById(R.id.imgBtnCorrect2);
+        ImageButton imgBtnInfo = findViewById(R.id.imgBtnInfo);
+        ImageButton imgBtnRasporedi = findViewById(R.id.imgBtnRaspored2);
+
+        imgBtnRasporedi.setBackgroundColor(ContextCompat.getColor(this, R.color.blue));
+        imgBtnInfo.setBackgroundColor(ContextCompat.getColor(this, R.color.blue));
         imgBtnWifi.setBackgroundColor(Color.RED);
         imgBtnBell.setBackgroundColor(Color.RED);
+        imgBtnConfirm.setBackgroundColor(Color.WHITE);
+        imgBtnConfirmRouter.setBackgroundColor(Color.WHITE);
+
         imgBtnWifi.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 imgBtnWifi.setBackgroundColor(Color.GREEN);
+            }
+        });
+        imgBtnRasporedi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(HomeActivity.this, Rasporedi.class);
+                startActivity(i);
             }
         });
     }
